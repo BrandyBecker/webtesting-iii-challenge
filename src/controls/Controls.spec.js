65928ba6601037186b0265365088506d5a36bec4
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-10-14 14:41:39 -0400

    + Initial Commit, + Snapshot Testing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+//Test away!
+import React from 'react';
+import {render} from '@testing-library/react';
+
+import Controls from './Controls';
+
+//create a snapshot of <Controls/>
+test('Controls renders correctly!', ()=>{
+    expect(render(<Controls/>)).toMatchSnapshot(); //creates snapshot after running test once, look in __snapshots__
+    render(<Controls/>);
+}) 
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,10 @@
-// Test away
+// Test away!
+import React from 'react';
+import {render} from '@testing-library/React';
+
+import Dashboard from './Dashboard';
+
+//create a snapshot of <Dashboard/>
+test('Dashboard renders correctly!',()=>{
+    expect(render(<Dashboard/>)).toMatchSnapshot(); //creates snapshot after running test once, look in __snapshots__
+})
